4b52caa4be994bbfbdb6d50ccd021b5dc8854ffe
Kraig Brockschmidt <kraigb@microsoft.com>
2020-09-23 12:30:22 -0700

    Remove secret key

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^l6-=gjr%o43(htf@04eohco$phxye&hijzrm5u#11q*gdjt6^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
