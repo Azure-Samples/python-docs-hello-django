a90ff2fd045513a2695b74c989eec947cde2c43e
Naveen Luke Fernando <jjnlfernando@gmail.com>
2021-06-20 11:53:11 +0530

    new

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World! Naveen")
+    return HttpResponse("Hello, World! Naveen from Azure")
